86e2fcf990e173a30124fd4f61b36613d86f09bb
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-09-05 13:39:24 +0000

    Downloads Events API almost done

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/interfaces/DownloadsEventsInterface.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/interfaces/DownloadsEventsInterface.java
@@ -1,9 +1,13 @@
 package org.jdownloader.myjdownloader.client.bindings.interfaces;
 
 import org.jdownloader.myjdownloader.client.bindings.ClientApiNameSpace;
+import org.jdownloader.myjdownloader.client.bindings.downloadlist.DownloadLinkQuery;
+import org.jdownloader.myjdownloader.client.json.DownloadListDiff;
 
 @ClientApiNameSpace("downloadevents")
 public interface DownloadsEventsInterface extends Linkable {
     public boolean setStatusEventInterval(long channelID, long interval);
+
+    DownloadListDiff queryLinks(DownloadLinkQuery query, int diffID);
 
 }
---
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/DownloadListDiff.java
@@ -0,0 +1,5 @@
+package org.jdownloader.myjdownloader.client.json;
+
+public class DownloadListDiff {
+
+}
