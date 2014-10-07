a9adecbabc58d9174e38652a9bde4b791fdc8d2c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-07 16:02:25 +0000

    build>full

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/exceptions/MyJDownloaderException.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/exceptions/MyJDownloaderException.java
@@ -3,40 +3,44 @@ package org.jdownloader.myjdownloader.client.exceptions;
 import org.jdownloader.myjdownloader.client.json.ErrorResponse.Source;
 
 public class MyJDownloaderException extends Exception {
-
+    
     private Source source;
-
+    
     public Source getSource() {
-        return source;
+        return this.source;
     }
-
+    
     public MyJDownloaderException(final String string) {
         super(string);
     }
-
+    
     public MyJDownloaderException() {
     }
-
+    
     public MyJDownloaderException(final Exception e) {
         super(e);
     }
-
+    
     public MyJDownloaderException(final String string, final Exception e) {
-        super(string,e);
+        super(string, e);
     }
-
+    
     public static MyJDownloaderException get(final Exception e) {
-        if (e instanceof MyJDownloaderException) {
-            return (MyJDownloaderException) e;
-        }
+        if (e instanceof MyJDownloaderException) { return (MyJDownloaderException) e; }
         return new MyJDownloaderException(e);
     }
-
-    public String toString() {
-        return super.toString() + "(SRC: " + getSource() + ")";
+    
+    public static MyJDownloaderException get(final Exception e, String message) {
+        if (e instanceof MyJDownloaderException) { return (MyJDownloaderException) e; }
+        return new MyJDownloaderException(message, e);
     }
-
+    
+    @Override
+    public String toString() {
+        return super.toString() + "(SRC: " + this.getSource() + ")";
+    }
+    
     public void setSource(final Source src) {
-        source = src;
+        this.source = src;
     }
 }
