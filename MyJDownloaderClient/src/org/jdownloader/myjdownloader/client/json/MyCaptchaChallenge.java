572458608349d9fc002780c0738b9a3c0c5e11c6
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-11-06 12:49:12 +0000

    build>full

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/MyCaptchaChallenge.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/json/MyCaptchaChallenge.java
@@ -1,47 +1,57 @@
 package org.jdownloader.myjdownloader.client.json;
 
 public class MyCaptchaChallenge {
-    
+
     public static enum TYPE {
         TEXT
     }
-    
-    private TYPE   type    = TYPE.TEXT;
-    private String source  = null;
+
+    private TYPE   type   = TYPE.TEXT;
+    private String source = null;
+    private String method = null;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
     private String dataURL = null;
     private String id      = null;
-    
+
     public MyCaptchaChallenge(/* Storable */) {
     }
-    
+
     public String getDataURL() {
         return this.dataURL;
     }
-    
+
     public final String getId() {
         return this.id;
     }
-    
+
     public String getSource() {
         return this.source;
     }
-    
+
     public TYPE getType() {
         return this.type;
     }
-    
+
     public void setDataURL(final String dataURL) {
         this.dataURL = dataURL;
     }
-    
+
     public final void setId(final String id) {
         this.id = id;
     }
-    
+
     public void setSource(final String source) {
         this.source = source;
     }
-    
+
     public void setType(final TYPE type) {
         this.type = type;
     }
