9e78841cbd9afa803b5e36901483453f09d9b5cb
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-11-10 16:28:15 +0000

    build>full

--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/AccountStorable.java
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/AccountStorable.java
@@ -3,83 +3,83 @@ package org.jdownloader.myjdownloader.client.bindings;
 import org.jdownloader.myjdownloader.client.json.AbstractJsonData;
 
 public class AccountStorable extends AbstractJsonData {
-    
+
     private Long    UUID = null;
     private boolean enabled;
-    
+
     private boolean valid;
-    
+
     private String  hostname;
-    
+
     private String  username;
     private String  errorType;
-    
+
     public String getErrorType() {
         return this.errorType;
     }
-    
+
     public void setErrorType(final String errorType) {
         this.errorType = errorType;
     }
-    
+
     public String getErrorString() {
         return this.errorString;
     }
-    
+
     public void setErrorString(final String errorString) {
         this.errorString = errorString;
     }
-    
+
     private String errorString;
     private Long   validUntil  = null;
-    
+
     private Long   trafficLeft = null;
     private Long   trafficMax  = null;
-    
+
     @SuppressWarnings("unused")
     protected AccountStorable(/* Storable */) {
     }
-    
+
     public String getHostname() {
         return this.hostname;
     }
-    
+
     public Long getTrafficLeft() {
         return this.trafficLeft;
     }
-    
+
     public Long getTrafficMax() {
         return this.trafficMax;
     }
-    
+
     public String getUsername() {
         return this.username;
     }
-    
+
     public Long getUUID() {
         return this.UUID;
     }
-    
+
     public Long getValidUntil() {
         return this.validUntil;
     }
-    
+
     public boolean isEnabled() {
         return this.enabled;
     }
-    
+
     public boolean isValid() {
         return this.valid;
     }
-    
+
     public void setEnabled(final boolean enabled) {
         this.enabled = enabled;
     }
-    
+
     public void setHostname(final String hostname) {
         this.hostname = hostname;
     }
-    
+
     public void setTrafficLeft(final Long trafficLeft) {
         if (trafficLeft == null) {
             this.trafficLeft = null;
@@ -87,29 +87,31 @@ public class AccountStorable extends AbstractJsonData {
             this.trafficLeft = Math.max(-1, trafficLeft);
         }
     }
-    
+
     public void setTrafficMax(final Long trafficMax) {
         if (trafficMax == null) {
             this.trafficMax = null;
         } else {
-            this.trafficMax = Math.max(-1, this.trafficLeft);
+
+            this.trafficMax = Math.max(-1, trafficMax);
+
         }
     }
-    
+
     public void setUsername(final String username) {
         this.username = username;
     }
-    
+
     public void setUUID(final Long uUID) {
         this.UUID = uUID;
     }
-    
+
     public void setValid(final boolean valid) {
         this.valid = valid;
     }
-    
+
     public void setValidUntil(final Long validUntil) {
         this.validUntil = validUntil;
     }
-    
+
 }
