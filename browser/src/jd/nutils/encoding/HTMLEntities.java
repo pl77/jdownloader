96f06d9bfab53f0a404dd204ba167e0ccf7d9eae
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-10 12:52:42 +0000

    HTMLEntities: use StringUtils.trim

--- browser/src/jd/nutils/encoding/HTMLEntities.java
+++ browser/src/jd/nutils/encoding/HTMLEntities.java
@@ -2,15 +2,14 @@ package jd.nutils.encoding;
 
 import java.util.Hashtable;
 
+import org.appwork.utils.StringUtils;
+
 /**
- * Collection of static methods to convert special and extended characters into
- * HTML entitities and vice versa.<br>
+ * Collection of static methods to convert special and extended characters into HTML entitities and vice versa.<br>
  * <br>
- * Copyright (c) 2004-2005 Tecnick.com S.r.l (www.tecnick.com) Via Ugo Foscolo
- * n.19 - 09045 Quartu Sant'Elena (CA) - ITALY - www.tecnick.com -
- * info@tecnick.com<br>
- * Project homepage: <a href="http://htmlentities.sourceforge.net"
- * target="_blank">http://htmlentities.sourceforge.net</a><br>
+ * Copyright (c) 2004-2005 Tecnick.com S.r.l (www.tecnick.com) Via Ugo Foscolo n.19 - 09045 Quartu Sant'Elena (CA) - ITALY - www.tecnick.com
+ * - info@tecnick.com<br>
+ * Project homepage: <a href="http://htmlentities.sourceforge.net" target="_blank">http://htmlentities.sourceforge.net</a><br>
  * License: http://www.gnu.org/copyleft/lesser.html LGPL
  * 
  * @author Nicola Asuni [www.tecnick.com].
@@ -25,11 +24,10 @@ public class HTMLEntities {
 
     /**
      * Translation table for HTML entities.<br>
-     * reference: W3C - Character entity references in HTML 4 [<a
-     * href="http://www.w3.org/TR/html401/sgml/entities.html"
+     * reference: W3C - Character entity references in HTML 4 [<a href="http://www.w3.org/TR/html401/sgml/entities.html"
      * target="_blank">http://www.w3.org/TR/html401/sgml/entities.html</a>].
      */
-    private static final Object[][] html_entities_table = { { "&Aacute;", 193 }, { "&aacute;", 225 }, { "&Acirc;", 194 }, { "&acirc;", 226 }, { "&acute;", 180 }, { "&AElig;", 198 }, { "&aelig;", 230 }, { "&Agrave;", 192 }, { "&agrave;", 224 }, { "&alefsym;", 8501 }, { "&Alpha;", 913 }, { "&alpha;", 945 }, { "&amp;", 38 }, { "&and;", 8743 }, { "&ang;", 8736 }, { "&Aring;", 197 }, { "&aring;", 229 }, { "&asymp;", 8776 }, { "&Atilde;", 195 }, { "&atilde;", 227 }, { "&Auml;", 196 }, { "&auml;", 228 }, { "&bdquo;", 8222 }, { "&Beta;", 914 }, { "&beta;", 946 }, { "&brvbar;", 166 }, { "&bull;", 8226 }, { "&cap;", 8745 }, { "&Ccedil;", 199 }, { "&ccedil;", 231 }, { "&cedil;", 184 }, { "&cent;", 162 }, { "&Chi;", 935 }, { "&chi;", 967 }, { "&circ;", 710 }, { "&clubs;", 9827 }, { "&cong;", 8773 }, { "&copy;", 169 }, { "&crarr;", 8629 }, { "&cup;", 8746 }, { "&curren;", 164 },
+    private static final Object[][]                 html_entities_table = { { "&Aacute;", 193 }, { "&aacute;", 225 }, { "&Acirc;", 194 }, { "&acirc;", 226 }, { "&acute;", 180 }, { "&AElig;", 198 }, { "&aelig;", 230 }, { "&Agrave;", 192 }, { "&agrave;", 224 }, { "&alefsym;", 8501 }, { "&Alpha;", 913 }, { "&alpha;", 945 }, { "&amp;", 38 }, { "&and;", 8743 }, { "&ang;", 8736 }, { "&Aring;", 197 }, { "&aring;", 229 }, { "&asymp;", 8776 }, { "&Atilde;", 195 }, { "&atilde;", 227 }, { "&Auml;", 196 }, { "&auml;", 228 }, { "&bdquo;", 8222 }, { "&Beta;", 914 }, { "&beta;", 946 }, { "&brvbar;", 166 }, { "&bull;", 8226 }, { "&cap;", 8745 }, { "&Ccedil;", 199 }, { "&ccedil;", 231 }, { "&cedil;", 184 }, { "&cent;", 162 }, { "&Chi;", 935 }, { "&chi;", 967 }, { "&circ;", 710 }, { "&clubs;", 9827 }, { "&cong;", 8773 }, { "&copy;", 169 }, { "&crarr;", 8629 }, { "&cup;", 8746 }, { "&curren;", 164 },
             { "&dagger;", 8224 }, { "&Dagger;", 8225 }, { "&darr;", 8595 }, { "&dArr;", 8659 }, { "&deg;", 176 }, { "&Delta;", 916 }, { "&delta;", 948 }, { "&diams;", 9830 }, { "&divide;", 247 }, { "&Eacute;", 201 }, { "&eacute;", 233 }, { "&Ecirc;", 202 }, { "&ecirc;", 234 }, { "&Egrave;", 200 }, { "&egrave;", 232 }, { "&empty;", 8709 }, { "&emsp;", 8195 }, { "&ensp;", 8194 }, { "&Epsilon;", 917 }, { "&epsilon;", 949 }, { "&equiv;", 8801 }, { "&Eta;", 919 }, { "&eta;", 951 }, { "&ETH;", 208 }, { "&eth;", 240 }, { "&Euml;", 203 }, { "&euml;", 235 }, { "&euro;", 8364 }, { "&exist;", 8707 }, { "&fnof;", 402 }, { "&forall;", 8704 }, { "&frac12;", 189 }, { "&frac14;", 188 }, { "&frac34;", 190 }, { "&frasl;", 8260 }, { "&Gamma;", 915 }, { "&gamma;", 947 }, { "&ge;", 8805 }, { "&harr;", 8596 }, { "&hArr;", 8660 }, { "&hearts;", 9829 }, { "&hellip;", 8230 }, { "&Iacute;", 205 },
             { "&iacute;", 237 }, { "&Icirc;", 206 }, { "&icirc;", 238 }, { "&iexcl;", 161 }, { "&Igrave;", 204 }, { "&igrave;", 236 }, { "&image;", 8465 }, { "&infin;", 8734 }, { "&int;", 8747 }, { "&Iota;", 921 }, { "&iota;", 953 }, { "&iquest;", 191 }, { "&isin;", 8712 }, { "&Iuml;", 207 }, { "&iuml;", 239 }, { "&Kappa;", 922 }, { "&kappa;", 954 }, { "&Lambda;", 923 }, { "&lambda;", 955 }, { "&lang;", 9001 }, { "&laquo;", 171 }, { "&larr;", 8592 }, { "&lArr;", 8656 }, { "&lceil;", 8968 }, { "&ldquo;", 8220 }, { "&le;", 8804 }, { "&lfloor;", 8970 }, { "&lowast;", 8727 }, { "&loz;", 9674 }, { "&lrm;", 8206 }, { "&lsaquo;", 8249 }, { "&lsquo;", 8216 }, { "&macr;", 175 }, { "&mdash;", 8212 }, { "&micro;", 181 }, { "&middot;", 183 }, { "&minus;", 8722 }, { "&Mu;", 924 }, { "&mu;", 956 }, { "&nabla;", 8711 }, { "&nbsp;", 160 }, { "&ndash;", 8211 }, { "&ne;", 8800 }, { "&ni;", 8715 },
             { "&not;", 172 }, { "&notin;", 8713 }, { "&nsub;", 8836 }, { "&Ntilde;", 209 }, { "&ntilde;", 241 }, { "&Nu;", 925 }, { "&nu;", 957 }, { "&Oacute;", 211 }, { "&oacute;", 243 }, { "&Ocirc;", 212 }, { "&ocirc;", 244 }, { "&OElig;", 338 }, { "&oelig;", 339 }, { "&Ograve;", 210 }, { "&ograve;", 242 }, { "&oline;", 8254 }, { "&Omega;", 937 }, { "&omega;", 969 }, { "&Omicron;", 927 }, { "&omicron;", 959 }, { "&oplus;", 8853 }, { "&or;", 8744 }, { "&ordf;", 170 }, { "&ordm;", 186 }, { "&Oslash;", 216 }, { "&oslash;", 248 }, { "&Otilde;", 213 }, { "&otilde;", 245 }, { "&otimes;", 8855 }, { "&Ouml;", 214 }, { "&ouml;", 246 }, { "&para;", 182 }, { "&part;", 8706 }, { "&permil;", 8240 }, { "&perp;", 8869 }, { "&Phi;", 934 }, { "&phi;", 966 }, { "&Pi;", 928 }, { "&pi;", 960 }, { "&piv;", 982 }, { "&plusmn;", 177 }, { "&pound;", 163 }, { "&prime;", 8242 }, { "&Prime;", 8243 },
@@ -39,12 +37,12 @@ public class HTMLEntities {
     /**
      * Map to convert extended characters in html entities.
      */
-    private static final Hashtable<Integer, String> htmlentities_map = new Hashtable<Integer, String>();
+    private static final Hashtable<Integer, String> htmlentities_map    = new Hashtable<Integer, String>();
 
     /**
      * Map to convert html entities in exteden characters.
      */
-    private static final Hashtable<String, Integer> unhtmlentities_map = new Hashtable<String, Integer>();
+    private static final Hashtable<String, Integer> unhtmlentities_map  = new Hashtable<String, Integer>();
 
     // ==========================================================================
     // ====
@@ -109,7 +107,9 @@ public class HTMLEntities {
      */
     public static String htmlentities(final String str) {
 
-        if (str == null) { return ""; }
+        if (str == null) {
+            return "";
+        }
         // initialize html translation maps table the first time is called
         if (HTMLEntities.htmlentities_map.isEmpty()) {
             HTMLEntities.initializeEntitiesTables();
@@ -191,9 +191,9 @@ public class HTMLEntities {
      */
     private static void initializeEntitiesTables() {
         // initialize html translation maps
-        for (int i = 0; i < HTMLEntities.html_entities_table.length; ++i) {
-            HTMLEntities.htmlentities_map.put((Integer) HTMLEntities.html_entities_table[i][1], (String) HTMLEntities.html_entities_table[i][0]);
-            HTMLEntities.unhtmlentities_map.put((String) HTMLEntities.html_entities_table[i][0], (Integer) HTMLEntities.html_entities_table[i][1]);
+        for (Object[] element : HTMLEntities.html_entities_table) {
+            HTMLEntities.htmlentities_map.put((Integer) element[1], (String) element[0]);
+            HTMLEntities.unhtmlentities_map.put((String) element[0], (Integer) element[1]);
         }
     }
 
@@ -233,8 +233,7 @@ public class HTMLEntities {
     }
 
     /**
-     * Convert HTML entities to special and extended unicode characters
-     * equivalents.
+     * Convert HTML entities to special and extended unicode characters equivalents.
      * 
      * @param str
      *            input string
@@ -242,7 +241,9 @@ public class HTMLEntities {
      * @see #htmlentities(String)
      */
     public static String unhtmlentities(final String str) {
-        if (str == null) { return null; }
+        if (str == null) {
+            return null;
+        }
 
         // initialize html translation maps table the first time is called
         if (HTMLEntities.htmlentities_map.isEmpty()) {
@@ -284,12 +285,11 @@ public class HTMLEntities {
                 buf.append(ch);
             }
         }
-        return buf.toString();
+        return StringUtils.trim(buf.toString());
     }
 
     /**
-     * Replace single and double quotes HTML entities with equivalent
-     * characters.
+     * Replace single and double quotes HTML entities with equivalent characters.
      * 
      * @param str
      *            the input string
@@ -298,7 +298,7 @@ public class HTMLEntities {
     public static String unhtmlQuotes(String str) {
         str = HTMLEntities.unhtmlDoubleQuotes(str); // convert double quotes
         str = HTMLEntities.unhtmlSingleQuotes(str); // convert single quotes
-        return str;
+        return StringUtils.trim(str);
     }
 
     /**
