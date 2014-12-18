0151117949c3f145d7408b628ab1674783b678ee
kotz <kotz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-18 13:33:36 +0000

    ExtractionAPI: added getArchiveInfo, cancelExtraction, startExtractionNow

---
+++ MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/interfaces/ExtractionInterface.java
@@ -0,0 +1,20 @@
+package org.jdownloader.myjdownloader.client.bindings.interfaces;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.jdownloader.myjdownloader.client.bindings.ArchiveStatusStorable;
+import org.jdownloader.myjdownloader.client.bindings.ClientApiNameSpace;
+
+@ClientApiNameSpace(ExtractionInterface.NAMESPACE)
+public interface ExtractionInterface extends Linkable {
+    public static final String NAMESPACE = "extraction";
+
+    public void addArchivePassword(String password);
+
+    public HashMap<String, Boolean> startExtractionNow(final long[] linkIds, final long[] packageIds);
+
+    public List<ArchiveStatusStorable> getArchiveInfo(final long[] linkIds, final long[] packageIds);
+
+    public Boolean cancelExtraction(long archiveId);
+}
