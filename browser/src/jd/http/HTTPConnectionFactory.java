ecf79aad5eb1febe28040352ed14169df66be2c3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-12 15:05:04 +0000

    SocketConnectionFactory: factory to create Sockets (eg via proxy)

--- browser/src/jd/http/HTTPConnectionFactory.java
+++ browser/src/jd/http/HTTPConnectionFactory.java
@@ -9,18 +9,26 @@ public class HTTPConnectionFactory {
     public static URLConnectionAdapter createHTTPConnection(final URL url, final HTTPProxy proxy) {
         if (proxy == null) {
             return new URLConnectionAdapterDirectImpl(url);
-        } else if (proxy.isPreferNativeImplementation()) {
-            return new URLConnectionAdapterNative(url, proxy);
-        } else if (proxy.isNone() || proxy.isDirect()) {
-            return new URLConnectionAdapterDirectImpl(url, proxy);
-        } else if (proxy.getType().equals(HTTPProxy.TYPE.SOCKS5)) {
-            return new URLConnectionAdapterSocks5Impl(url, proxy);
-        } else if (proxy.getType().equals(HTTPProxy.TYPE.SOCKS4)) {
-            return new URLConnectionAdapterSocks4Impl(url, proxy);
-        } else if (proxy.getType().equals(HTTPProxy.TYPE.HTTP)) {
-            return new URLConnectionAdapterHTTPProxyImpl(url, proxy);
         } else {
-            throw new RuntimeException("unsupported proxy type: " + proxy.getType().name());
+            if (proxy.isPreferNativeImplementation()) {
+                return new URLConnectionAdapterNative(url, proxy);
+            } else {
+                switch (proxy.getType()) {
+                case DIRECT:
+                    return new URLConnectionAdapterDirectImpl(url, proxy);
+                case HTTP:
+                    return new URLConnectionAdapterHTTPProxyImpl(url, proxy);
+                case NONE:
+                    return new URLConnectionAdapterDirectImpl(url, proxy);
+                case SOCKS4:
+                    return new URLConnectionAdapterSocks4Impl(url, proxy);
+                case SOCKS5:
+                    return new URLConnectionAdapterSocks5Impl(url, proxy);
+                default:
+                    throw new RuntimeException("unsupported proxy type: " + proxy.getType().name());
+                }
+            }
         }
     }
+
 }
---
+++ browser/src/jd/http/SocketConnectionFactory.java
@@ -0,0 +1,38 @@
+package jd.http;
+
+import java.net.Socket;
+
+import org.appwork.utils.net.httpconnection.HTTPProxy;
+import org.appwork.utils.net.httpconnection.SocksHTTPconnection.DESTTYPE;
+import org.appwork.utils.net.socketconnection.DirectSocket;
+import org.appwork.utils.net.socketconnection.Socks4Socket;
+import org.appwork.utils.net.socketconnection.Socks5Socket;
+
+public class SocketConnectionFactory {
+
+    public static Socket createSocket(final HTTPProxy proxy) {
+        if (proxy == null) {
+            return new DirectSocket();
+        } else {
+            if (proxy.isPreferNativeImplementation()) {
+                /* TODO */
+                return new Socket();
+            } else {
+                switch (proxy.getType()) {
+                case DIRECT:
+                    return new DirectSocket(proxy);
+                case HTTP:
+                    return null;
+                case NONE:
+                    return new DirectSocket();
+                case SOCKS4:
+                    return new Socks4Socket(proxy, DESTTYPE.DOMAIN);
+                case SOCKS5:
+                    return new Socks5Socket(proxy, DESTTYPE.DOMAIN);
+                default:
+                    throw new RuntimeException("unsupported proxy type: " + proxy.getType().name());
+                }
+            }
+        }
+    }
+}
